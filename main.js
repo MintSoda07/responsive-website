d64c0f39a259b9146cf16d2e1e33e30b6c955ca6
MintSoda07 <79971109+MintSoda07@users.noreply.github.com>
2021-11-03 16:35:45 +0900

    JS navbar event added 1
    navbar color changing by scroll event added.

---
+++ main.js
@@ -0,0 +1,14 @@
+window.onload=function(){
+    // window load
+
+}
+'use strict';
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener("scroll",()=>{
+    if(window.scrollY<navbarHeight){
+        navbar.classList.add('navbar--dark');
+    }else{
+        navbar.classList.remove('navbar--dark');
+    }
+})
