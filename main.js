93f990cf104ffe13c14a991682bcda7f42e8e331
MintSoda07 <79971109+MintSoda07@users.noreply.github.com>
2021-11-17 17:36:37 +0900

    Category_Btn JS added

--- main.js
+++ main.js
@@ -66,3 +66,37 @@ navbarToggleBtn.addEventListener("click",()=>{
     console.log("Clicked!");
     navbarMenuID.classList.toggle("open");
 });
+
+const workBtnContainer = document.querySelector(".work_categories");
+const projectContainer = document.querySelector(".work_projects");
+const projects = document.querySelectorAll(".project");
+workBtnContainer.addEventListener("click", (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if (filter == null) {
+        return;
+    }
+    projects.forEach((project) => {
+        if (filter === "*" || filter === project.dataset.type) {
+            project.classList.remove("invisible");
+        } else {
+            project.classList.add("invisible");
+        }
+    });
+    const active = document.querySelector(".category_btn.selected");
+    if (active != null) {
+        active.classList.remove("selected");
+    }
+    e.target.classList.add("selected");
+    projectContainer.classList.add("anim-out");
+    setTimeout(() => {
+        projects.forEach((project) => {
+            console.log(project.dataset.type);
+            if (filter === "*" || filter === project.dataset.type) {
+                project.classList.remove("invisible");
+            } else {
+                project.classList.add("invisible");
+            }
+        });
+        projectContainer.classList.remove("anim-out");
+    }, 300);
+});
